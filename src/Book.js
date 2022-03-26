4d5fde335ecce21d4117dc1895b4cd4acae62b46
Marwan Shehata <marwanzeitoun1@gmail.com>
2022-03-26 23:53:22 +0200

    Added Functionality

--- src/Book.js
+++ src/Book.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import ShelfChanger from "./ShelfChanger";
+import * as BooksAPI from "./BooksAPI";
 
-const Book = ({ book, title, authors, thumbnail, refreshShelves, shelf }) => {
-  const authorList = authors ? authors : ["Author Unknown"];
-  const thumb = thumbnail
-    ? thumbnail
-    : "https://via.placeholder.com/128x193?text=No%Cover";
+export default class Book extends Component {
+  // I tried to add the update function to the componentDidMount method, but it didn't work.
+  // Can't figure out why.
+  // Isn't all promises and async functions must be handled in the componentDidMount method?
+  // So instead I broke it into separate methods outside of the componentDidMount method (changeShelf)
+  // getAll() method returns a promise, so we can use async/await
+  shelfSetter = () => {
+    BooksAPI.getAll()
+      .then((response) => this.setState({ books: response, error: false }))
+      .catch((error) => this.setState({ error: true }));
+  };
+  render() {
+    const { book, title, authors, shelf } = this.props;
+    const authorList = authors ? authors : ["Author Unknown"];
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div
-          className="book-cover"
-          style={{
-            width: 128,
-            height: 193,
-            backgroundImage: `url(${thumb})`,
-          }}
-        />
-        {refreshShelves ? (
-          <ShelfChanger
-            book={book}
-            refreshShelves={refreshShelves}
-            shelf={shelf}
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage:
+                book.imageLinks && book.imageLinks.thumbnail
+                  ? `url(${book.imageLinks.thumbnail})`
+                  : `url("https://loremflickr.com/128/193")`, // Or we can use a placeholder image from the web app's server
+            }}
           />
-        ) : (
-          <ShelfChanger book={book} shelf={shelf} />
-        )}
+          {this.shelfSetter ? (
+            <ShelfChanger
+              book={book}
+              refreshShelves={this.shelfSetter}
+              shelf={shelf}
+            />
+          ) : (
+            <ShelfChanger book={book} shelf={shelf} />
+          )}
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authorList.join(", ")}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{authorList.join(", ")}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Book;
+// const Book = ({ book, title, authors, thumbnail, refreshShelves, shelf }) => {
+//   const authorList = authors ? authors : ["Author Unknown"];
+//   refreshShelves = () => {
+//     BooksAPI.getAll()
+//       .then((response) =>
+//         this.setState({ shelvedBooks: response, error: false })
+//       )
+//       .catch((error) => this.setState({ error: true }));
+//   };
+
+//   const thumb = thumbnail
+//     ? thumbnail
+//     : "https://via.placeholder.com/128x193?text=No%Cover";
+
+//   return (
+
+//   );
+// };
+
+// export default Book;
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -5,14 +5,81 @@ import Book from "../Book";
 
 class SearchBooks extends Component {
   state = {
+    queryInput: "",
     books: [],
-    query: "",
-    shelvedBooks: this.props.location.state
+    query: "" /* this should be remove */,
+    searchedBooks: [],
+  };
+  searchMethods = {
+    async userInput(input) {
+      // searchQuery(){}
+      if (input.length > 0) {
+        try {
+          const books = await BooksAPI.search(input);
+          if (books.id === (null || undefined)) {
+            this.setState({ searchedBooks: [] });
+          } else {
+            this.setState({ searchedBooks: books });
+          }
+        } catch (error) {
+          console.log(
+            "error retriving searched data. Please check your API request. Source: Search.js",
+            error
+          );
+        }
+      } else {
+        this.setState({ searchedBooks: [] });
+      } // if I make changeInputQuery an arrow function, it won't work
+    },
+
+    changeInputQuery(event) {
+      // handleChange(){}
+      //  https://stackoverflow.com/questions/67014481/what-is-event-target-value-in-react-exactly
+      //  event here is is SynthenticEvent that is a wrapper around the native event
+      //  https://reactjs.org/docs/events.html
+      // console.log("clg event before", event);
+      // console.log("clg event.target before", event.target);
+      // console.log("clg event.target.value before", event.target.value);
+      // console.log("clg event.target.queryInput before", event.target.queryInput);
+
+      const queryInput = event.target.queryInput; // or event.target.value is the value of the input
+      // console.log("clg event after", event);
+      // console.log("clg event.target after", event.target);
+      // console.log("clg event.target.value after", event.target.value);
+      // console.log("clg event.target.queryInput after", event.target.queryInput);
+      this.setState({ queryInput: queryInput }, () => {
+        this.userInput(queryInput);
+      }); // if I make changeInputQuery an arrow function, it won't work
+    },
+    removeResults() {
+      this.setState({ searchedBooks: [] });
+    },
   };
 
-  searchBooks = query => {
+  changeShelf = (book, shelf) => {
+    if (this.state.error !== true) {
+      BooksAPI.update({ id: book.id }, shelf).then((metaData) => {
+        if (metaData) {
+          this.setState((state) => ({
+            books: state.books.map((mappedBook) => {
+              if (mappedBook.id === book.id) {
+                mappedBook.shelf = shelf;
+              }
+              // we can instead use Object.assign({}, mappedBook, { shelf: shelf })
+              // or filter the books array and chaining it with concat
+              return mappedBook;
+            }),
+          }));
+        }
+      });
+    }
+  };
+
+  ///////////////////////////////////////////////////////
+
+  searchBooks = (query) => {
     if (query !== "") {
-      BooksAPI.search(query).then(response => {
+      BooksAPI.search(query).then((response) => {
         if (!response.error) {
           this.setState({ books: response });
         } else {
@@ -24,24 +91,27 @@ class SearchBooks extends Component {
     }
   };
 
-  handleChange = newQuery => {
+  handleChange = (newQuery) => {
     this.setState({ query: newQuery });
     this.searchBooks(newQuery);
   };
 
-  getShelfName = book => {
-    const result = this.state.shelvedBooks.filter(
-      shelvedBook => shelvedBook.id === book.id
+  getShelfName = (book) => {
+    const result = this.state.searchedBooks.filter(
+      (shelvedBook) => shelvedBook.id === book.id
     );
     return result.length !== 0 ? result[0].shelf : "none";
   };
   render() {
-    const { books } = this.state;
+    const { books, queryInput, searchedBooks } = this.state; // added queryInput and searchedBooks
+    const { changeShelf } = this;
+    const { changeInputQuery, userInput, removeResults } = this.searchMethods;
 
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/">
+            {/* className didnt work on Link for some reason so I added a button instead */}
             <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
@@ -49,14 +119,17 @@ class SearchBooks extends Component {
               type="text"
               placeholder="Search by title or author"
               value={this.state.query}
-              onChange={event => this.handleChange(event.target.value)}
+              onChange={(event) => this.handleChange(event.target.value)}
             />
+            {/* The input autofocus attribute specifies that an input field
+                should automatically get focus when the page loads. "autoFocus"
+                instead of autofocus in HTML */}
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
             {books.length !== 0 &&
-              books.map(book => (
+              books.map((book) => (
                 <Book
                   key={book.id}
                   book={book}
